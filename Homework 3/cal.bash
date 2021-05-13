e89260f9ffa98f76917d0602d3f119c5ac50e67d
Dai Vuong <66444176+paulminhdai@users.noreply.github.com>
2021-05-12 18:00:29 -0700

    Update cal.bash

--- Homework 3/cal.bash
+++ Homework 3/cal.bash
@@ -1,18 +1,32 @@
 #!/bin/bash
 #---------------------------------------------------------------------
 # AUTHOR: Dai (Paul) Vuong
-# FILENAME: cal.bash
+# FILENAME: cal.sh
 # SPECIFICATION: This program displays calendar of a specific month. 
 # FOR: CS 2600- Homework #3
 # TIME SPENT: 2 hours
 #--------------------------------------------------------------------*/
 
+daysForFebuary()
+{
+    year=$1
+
+    if [ $((year % 400)) -eq 0 ] # Check for Leap year
+        then
+            return 29
+        elif [ $(($year % 4 )) -eq 0 -a $(($year % 400)) -ne 0 ]
+        then
+            return 29
+        else
+			return 28
+		fi
+}
 
 daysInMonth()
 {
-  # This method return the number of days in a specific month
-  # Params: month, year
-  # Return: number of days
+    # This method return the number of days in a specific month
+    # Params: month, year
+    # Return: number of days
 
 	monthInt=$1
 	year=$2
@@ -24,15 +38,9 @@ daysInMonth()
 	
 	if [ "$monthInt" == 2 ] #Feb
     then        
-		if [ $((year % 400)) -eq 0 ] # Check for Leap year
-        then
-            return 29
-        elif [ $(($year % 4 )) -eq 0 -a $(($year % 400)) -ne 0 ]
-        then
-            return 29
-        else
-			return 28
-		fi
+		daysForFebuary $year
+        day=$?
+        return $day
 	fi
 	
 	if [ "$monthInt" == 3 ] # Mar
@@ -70,13 +78,19 @@ daysInMonth()
 
 firstDay()
 {
-  # This method return the first day of month
-  # Params: year
-  # Return: day
-	year=$1
-	temp=$((year - 1)) 
-    ((res = ($year + $temp/4 - $temp/100 + $temp/400) % 7))
-	return $res
+    # This method return the first day of month
+    # Params: year
+    # Return: day
+    day=$1
+    month=$2
+    year=$3
+
+    (( y = $year - (14 - $month) / 12 ))
+    (( x = $y + $y/4 - $y/100 + $y/400 ))
+    (( m = $month + 12 * ((14 - $month) / 12) - 2 ))
+    (( d = ($day + $x + (31*$m) / 12) % 7 ))
+
+    return $d
 }
 
 header()
@@ -93,54 +107,6 @@ header()
     echo " Su Mo Tu We Th Fr Sa"
 }
 
-printCal()
-{
-    # This method print out all days of the month
-    # Params: month, year
-    # Return: none
-    year=$1
-    month=$2
-
-    firstDay $year # call the firstDay method
-    current=$?
-
-    for (( i = 1; i < 13; i++ )) # Go through 12 months
-    do
-        if [ $i -eq $month ]
-        then
-            header $i $year # print out the header
-        fi
-
-        daysInMonth $i $year # call the daysInMonth method
-        days=$?
-
-        for (( curr = 0; curr < current; curr++ ))
-        do            
-            if [ $i -eq $month ]
-            then
-                echo -n "   " # print blank space before the first day of the month
-            fi
-        done
-
-        for (( j = 1; j <= days; j++ ))
-        do
-            if [ $i -eq $month ]
-            then
-                printf "%3d" "$j" # print days of the month
-            fi           
-            
-            if (( ++curr > 6 ))
-            then
-                (( curr = 0 ))
-                if [ $i -eq $month ] # go next line after a week
-                then
-                    echo
-                fi
-            fi
-        done
-        current=$curr
-    done    
-}
 
 # Main
 
@@ -181,4 +147,28 @@ if [ $year -lt 100 ]; then
     (( year = year + 2000 ))
 fi
 
-printCal $year $month
+
+header $month $year # call header
+
+# Start printing days
+firstDay 1 $month $year
+d=$?
+
+for (( i = 0; i < $d; i++ ))
+do
+    echo -n "   " # print white space before the first day of the month
+done
+
+daysInMonth $month $year
+days=$?
+
+for (( i = 1; i <= $days; i++))
+do
+    printf "%3d" "$i" # print out days
+    (( temp = ($i + $d) % 7 ))
+    if [[ $temp  == 0  || $i == $days ]] # go to next line
+    then
+        echo 
+    fi
+done
+
